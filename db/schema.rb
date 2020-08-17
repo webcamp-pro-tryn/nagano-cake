1ee9d65f690fa65b5bff66124716aef4edf9224e
vagrant <vagrant@localhost.localdomain>
2020-08-17 11:56:36 +0000

    customer login機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_054042) do
+ActiveRecord::Schema.define(version: 2020_08_17_085745) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_08_17_054042) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
