06f54f053bb81cc137c73d81f62604d0e69d6d68
Takumi Endo <endotaku0608@gmail.com>
2020-08-18 03:22:25 +0000

    customersフォルダ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	# before_action :authenticate_customer!, except: [:top, :about]
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :is_deleted])
+	end
 end
