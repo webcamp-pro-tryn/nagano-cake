c67220ee1bc1a25a6c88b1ed9875b3d9b64f7e50
vagrant <vagrant@localhost.localdomain>
2020-08-19 04:31:31 +0000

    test

---
+++ app/controllers/host/customers_controller.rb
@@ -0,0 +1,12 @@
+class Host::CustomersController < ApplicationController
+  def index
+  	@customers = Customers.all
+  end
+
+  def show
+  	@customer =Customers.find(params[:id])
+  end
+
+  def edit
+  end
+end
