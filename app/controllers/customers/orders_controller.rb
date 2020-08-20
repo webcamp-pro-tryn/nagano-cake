0d447f243dfcba4d836b914fc9c47d3044736dbd
Takumi Endo <endotaku0608@gmail.com>
2020-08-20 12:03:44 +0000

    order new confirm

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,10 +1,27 @@
 class Customers::OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
 
-	def index
-		@orders = Order.all
-	end
+  def index
+    @orders = Order.all
+  end
 
-	def show
-	end
-	
+  def show
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+  end
+
+  def confirm
+    @cart_items = CartItem.all
+    @order = Order.find_by(customer_id: current_user.id)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment, :name, :postal_code, :address)
+  end
 end
