e632c6470d41752809699f449cfcdc64990a2d1f
vagrant <vagrant@localhost.localdomain>
2020-08-22 11:45:13 +0000

    enum setting

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails'
+gem 'enum_help'
+gem 'rails-i18n'
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
-	#has_many :cart_items, dependent: :destroy
+	has_many :order_items, dependent: :destroy
 	belongs_to :customer
+	enum payment: {credit: 0, cach: 1}, _prefix: true
+	enum order_status: {waiting: 0, check: 1, maiking: 2, preparation: 3, complete: 4}, _prefix: true
+
+	 
+        
 end
